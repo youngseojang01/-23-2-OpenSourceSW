313793eda187fa45074d450aef3658b5fa9d7c0e
yongyong <youngseojang@gmail.com>
2023-10-26 01:21:25 +0900

    3번: rating 필드의 평균 출력

--- test.sh
+++ test.sh
@@ -2,16 +2,16 @@
 read -p "User Name: " name
 read -p "Student Number: " std_num
 echo "[ MENU ] 
-	1. Get the data of the movie identified by a specific 'movie id' from 'u.item'
-	2. Get the data of 'action' genre movies from 'u.item'
-	3. Get the average 'rating' of the movie identified by specific 'moive id' from 'u.data'
-	4. Delete the 'IMDb URL' from 'u.item'
-	5. Get the data about movies rated by a specific 'user id' from 'u.data'
-	6. Modify the format of movies rated by a specific 'user id' from 'u.data'
-	7. Get the data of movies rated by a specific 'user id' from 'u.data'
-	8. Get the average 'rating' of movies rated by users with 'age' between 20 and 29 and 'occupation' as 'programmer'
-	9. Exit"
-echo "-------------------------"
+1. Get the data of the movie identified by a specific 'movie id' from 'u.item'
+2. Get the data of 'action' genre movies from 'u.item'
+3. Get the average 'rating' of the movie identified by specific 'moive id' from 'u.data'
+4. Delete the 'IMDb URL' from 'u.item'
+5. Get the data about movies rated by a specific 'user id' from 'u.data'
+6. Modify the format of movies rated by a specific 'user id' from 'u.data'
+7. Get the data of movies rated by a specific 'user id' from 'u.data'
+8. Get the average 'rating' of movies rated by users with 'age' between 20 and 29 and 'occupation' as 'programmer'
+9. Exit"
+echo "-------------------------------------------"
 read -p "Enter your choice [ 1-9 ] " choice
 echo "choice: $choice"
 case $choice in
@@ -30,7 +30,7 @@ case $choice in
 	3)
 		read -p "Please enter the 'movie id' (1~1682): " movie_id
 		echo "average rating of $movie_id: "
-		cat u.data | awk -F\| -v id=$movie_id sum=0 '$2==id { sum+=$3 }' | echo sum
+		cat u.data | awk -v id=$movie_id '$2==id { sum+=$3; cnt++ } END { print sum/cnt }'
 		;;
 	4)
 		read -p "Do you want to delete the 'IMDb URL' from 'u.item'?(y/n): " respond
