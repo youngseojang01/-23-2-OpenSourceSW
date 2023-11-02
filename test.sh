8b83820a2ab39da6e684f8f1209331c5707fe0e6
yongyong <youngseojang@gmail.com>
2023-11-03 02:35:06 +0900

    수정: 줄 개행, 파일 입력, 이름학번

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-read -p "User Name: " name
-read -p "Student Number: " std_num
+item=$1
+data=$2
+user=$3
+read "User Name: 장영서"
+read "Student Number: 12201790"
 echo "[ MENU ] 
 1. Get the data of the movie identified by a specific 'movie id' from 'u.item'
 2. Get the data of 'action' genre movies from 'u.item'
@@ -16,58 +19,46 @@ for((;;))
 do
 read -p "Enter your choice [ 1-9 ] " choice
 case $choice in
-	1) 
-		echo "
-		"
+	1)
+		echo -e "\n"
 		read -p "Please enter the 'movie id' (1~1682): " movie_id
-		cat u.item | awk -F\| -v id=$movie_id '$1==id {print $0}'
-		echo"
-		"
+		awk -F\| -v id=$movie_id '$1==id { print $0 }' $item
 		;;
 	2)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Do you want to get the data of 'action' genre movies from 'u.item'? (y/n): " respond
 		if [ $respond == "y" ] 
 		then
-			cat u.item | awk -F\| '$7==1 {print $1, $2}' | head
+			awk -F\| '$7==1 {print $1, $2}' $item | head
 		fi
-		echo"
-		"
 		;;
 	3)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Please enter the 'movie id' (1~1682): " movie_id
 		echo "average rating of $movie_id: "
-		cat u.data | awk -v id=$movie_id '$2==id { sum+=$3; cnt++ } END { print sum/cnt }'
-		echo "
-		"
+		awk -v id=$movie_id '$2==id { sum+=$3; cnt++ } END { print sum/cnt }' $data
 		;;
 	4)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Do you want to delete the 'IMDb URL' from 'u.item'?(y/n): " respond
 		if [ $respond == "y" ]
 		then
-			sed -i '/^http://us.imdb.com/d' u.item
+			sed -i '/^http://us.imdb.com/d' $item
 			sed -E "s/http[^|]*\)//" 
-			cat u.item | awk -F\| '{print $0}' | head
+			awk -F\| '{print $0}' $item | head
 		fi
 		;;
 	5)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Do you want to get the data about users from 'u.user'?(y/n): " respond
 		if [ $respond == "y" ]
 		then
-			cat u.user | awk -F\| '{print "user ", $1, " is ", $2, " years old " $3, $4}' | head
+			awk -F\| '{print "user ", $1, " is ", $2, " years old " $3, $4}' $user | head
 	
 		fi
 		;;
 	6)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Do you want to modify the format of 'release data' in 'u.item'?(y/n): " respond
 		if [ $respond == "y" ] 
 		then
@@ -84,39 +75,34 @@ case $choice in
 			s/Oct/10/g
 			s/Nov/11/g
 			s/Dec/12/g
-			s/-//g' u.item |
+			s/-//g' $item |
 			sed -E -e '
 			s/([0-9]{2})([0-9]{2})([0-9]{4})/\3\2\1/g
 			' | tail
 		fi
 		;;
 	7)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Please enter the 'user id' (1~943): " user_id
-		cat u.data | awk -v id=$user_id '$1==id { print $2 }' | sort -n | sed -z 's/\n/|/g' | head
-		echo "
-		"
-		cat u.data | awk -v id=$user_id '$1==id { print $2 }' | sort -n | head |
+		awk -v id=$user_id '$1==id { print $2 }' $data | sort -n | sed -z 's/\n/|/g' | head
+		echo -e "\n"
+		awk -v id=$user_id '$1==id { print $2 }' $data | sort -n | head |
 			while read line
 			do
-				awk -F\| -v id=$line '$1==id { print $1, "|", $2 }' u.item
+				awk -F\| -v id=$line '$1==id { print $1, "|", $2 }' $item
 			done
-		echo "
-		"
 		;;
 	8)
-		echo "
-		"
+		echo -e "\n"
 		read -p "Do you want to get the average 'rating' of movies rated by users with 'age' between 20 and 29 and 'occupation' as 'programmer'? (y/n): " respond
 		if [ $respond == "y" ]
 		then
-			for var in $(seq 1 10)
+			for var in $(seq 1 1682)
 			do
-			awk -F\| -v occupation="programmer" '$2 >=20 && $2 < 30 && $4==occupation { print $1 }' u.user |
+			awk -F\| -v occupation="programmer" '$2 >=20 && $2 < 30 && $4==occupation { print $1 }' $user |
 				while read line
 				do
-					awk -v user_id=$line '$1==user_id { print $0 }' u.data 
+					awk -v user_id=$line '$1==user_id { print $0 }' $data 
 				done | awk -v id=$var '$2==id { sum+=$3; cnt++ } END { print id, sum/cnt }'
 			done
 		fi
@@ -126,6 +112,7 @@ case $choice in
 		break
 		;;
 	*)
-		echo "wrong choice"
+		echo "Wrong choice!"
 	esac
+	echo -e "\n"
 done
